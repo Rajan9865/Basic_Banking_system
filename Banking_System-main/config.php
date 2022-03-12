09125f6c29e7ec3e76f941c0058b5c8ee234fdae
Rajan9865 <81039046+Rajan9865@users.noreply.github.com>
2022-03-12 13:18:12 +0530

    Add files via upload
    I have created Basic banking system
    #gripmar22
    Task1: Basic banking syystem
    The basic banking System website frunction is to transfer from one account to another account .
    ***  FrontEnd**
    HTML,CSS,BOOTSTRAP,JAVASCRIPT
    ** BACKEND**
    PHP
    DATABASE :MSQL

---
+++ Banking_System-main/config.php
@@ -0,0 +1,20 @@
+<?php
+
+	//$conn = mysqli_connect('localhost','root','','bank');
+	$db_host = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "sparks_bank";
+$db_port = 3307;
+
+// Create Connection
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name, $db_port);
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
+
+
+
